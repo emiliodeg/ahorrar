e0ed1b27c397839805926d484fa1923340eda62a
Emilio <emiliodeg@gmail.com>
2025-06-13 16:50:43 -0300

    refactor: remove unused demo components and routes from UIKit
    - Deleted TableDemo, TimelineDemo, TreeDemo, and their respective routes.
    - Removed the Vercel configuration file as it is no longer needed.

--- src/app/pages/about-page/about-page.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'ahorrar-about-page',
-  template: `<p>about-page works!</p>`,
-  styleUrl: './about-page.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export default class AboutPage {}
