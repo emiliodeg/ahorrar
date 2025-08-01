1cdf2b3ef921f3975fc7e3b341d103eafae61fa2
Emilio <1172386+emiliodeg@users.noreply.github.com>
2025-08-01 10:12:28 -0300

    added packages and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-import PrimeUI from 'tailwindcss-primeui';
-
-export default {
-  darkMode: ['selector', '[class="app-dark"]'],
-  content: ['./src/**/*.{html,ts,scss,css}', './index.html'],
-  plugins: [PrimeUI],
+module.exports = {
+  content: ["./src/**/*.{html,ts}",],
   theme: {
-    screens: {
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
-      '2xl': '1920px'
-    }
-  }
-};
+    extend: {},
+  },
+  plugins: [],
+}
