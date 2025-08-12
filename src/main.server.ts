172f62b4bc3c805b1fb5cea2d2339dc85fdb89bd
Emilio <1172386+emiliodeg@users.noreply.github.com>
2025-08-12 18:17:08 -0300

    removed ssr

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { config } from './app.config.server';
-import { AppComponent } from './app.component';
-
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
