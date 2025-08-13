467c3839936c657cb6a2e7e771e7657143c5d706
Emilio <1172386+emiliodeg@users.noreply.github.com>
2025-08-13 16:39:00 -0300

    changed import

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+// From a forum post.  It didnt work.
+// if (typeof global === undefined) {
+//   globalThis.global = globalThis
+// }
+
+import { CommonEngine } from '@angular/ssr/node';
+import { render } from '@netlify/angular-runtime/common-engine';
+
+const commonEngine = new CommonEngine();
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  return await render(commonEngine);
+}
