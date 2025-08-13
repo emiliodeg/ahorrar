77d2d1ca4866598aafd8e6f78d0b4f728bd44e13
Emilio <1172386+emiliodeg@users.noreply.github.com>
2025-08-13 14:55:59 -0300

    env fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 import { Environment } from './environment.model';
 
 export const environment: Environment = {
-  production: process.env['NODE_ENV'] === 'production',
-  baseUrl: process.env?.['BASE_URL'] || '',
+  production: process?.env['NODE_ENV'] === 'production',
+  baseUrl: process?.env['BASE_URL'] || '',
   supabase: {
-    apiUrl: process.env?.['SUPABASE_DATABASE_URL'] || '',
-    key: process.env?.['SUPABASE_ANON_KEY'] || ''
+    apiUrl: process?.env['SUPABASE_DATABASE_URL'] || '',
+    key: process?.env['SUPABASE_ANON_KEY'] || ''
   }
 };
